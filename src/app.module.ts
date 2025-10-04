e76f468d902a259f9671cb7e755d46a6a3d7348d
Sin@n2007 <ms.sagir@student.avans.nl>
2025-10-04 23:58:29 +0200

    works

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
