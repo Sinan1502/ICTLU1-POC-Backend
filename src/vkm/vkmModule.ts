8a4327c82de17e26b61c0522473cad8225423ab8
Sin@n2007 <ms.sagir@student.avans.nl>
2025-10-08 14:20:45 +0200

    works en creat src vkm

---
+++ src/vkm/vkmModule.ts
@@ -0,0 +1,12 @@
+{
+interface VkmModule {
+    id: string,
+    name: string,
+    studycredit: number,
+    location: string,
+    level: string,
+    interests_match_score: number,
+    available_spots: number,
+    start_date: string
+}
+}
