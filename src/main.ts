f930eb26012ad223e1c78e479b4edd97fcb1b5ef
Sin@n2007 <ms.sagir@student.avans.nl>
2025-10-16 12:50:49 +0200

    works

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://ictlu1-poc-backend.onrender.com', // ← Vervang met je echte Render URL!
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
